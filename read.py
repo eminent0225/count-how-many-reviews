732844facb1127f1eec2694c9cf67a28f4aad718
Anthony <eminent0225@gmail.com>
2020-06-27 22:59:24 +0800

    searching for good

--- read.py
+++ read.py
@@ -15,7 +15,30 @@ with open("reviews.txt","r") as f :    ##其中r 是讀取w是寫
 
 print("檔案已讀取完","共有",len(data),"筆資料")
 n = 3
-while n < 7:
+while n < 5:
 	print(data[n])
 	print("------------------------")
 	n += 1
+##計算平均留言字數
+sum_len = 0
+for d in data :
+	sum_len += len(d)
+print("平均留言字數為: ",sum_len/len(data))
+
+newdata = []
+for d in data :
+	if len(d) < 100:
+		newdata.append(d)
+
+dd = 0
+while dd <5 :
+	print(newdata[dd])
+	print("------------------------")
+	dd += 1
+
+good = []
+for ddd in data: # 一定得提取 不然無法收集所需要的資料 進行整理
+	if "good" in ddd:
+		good.append(ddd)
+print("總共有",len(good),"留言包含good")
+print(good[2])
